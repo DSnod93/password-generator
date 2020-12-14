4f2e5ef8949ea84c8226ff5f5694e461d9dd61ce
DSnoddy <douglassnoddyjr@gmail.com>
2020-12-13 19:40:09 -0800

    Finalized function for password generator

--- Develop/script.js
+++ Develop/script.js
@@ -2,61 +2,85 @@
 
 
 // Create variables to store uppercase letters lowercase letters numbers and special characters
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var number = "1234567890";
-var specialCharacter = "~!@#$%^&*()_+=";
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var number = [0,1,2,3,4,5,6,7,8,9];
+var specialCharacter = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','['];
+
+// Variables to verify user selection if they want uppercase letters, lowercase letters, numbers, and special characters
+var upperSelection = false;
+var lowerSelection = false;
+var numberSelection = false;
+var specialSelection = false;
+var passwordLength = [];
+var pw = [];
 
 
-function userChoice() {
+function generatePassword() {
+
   // Ask user how long do they want their password to be
-  var characterLength = parseInt(prompt("Enter a number between 8 and 128"));
+  var characterLength = prompt("Enter a number between 8 and 128");
+  var passwordLength = [];
 
-  if (characterLength < 8 || characterLength > 128) {
-    window.prompt("Value must be between 8 and 128");
+  // Verify user input value between 8 and 128
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    var characterLength = window.prompt("What length would you like your password to be? Enter number between 8 and 128");
+    if (characterLength === null)
+      return;
   }
 
-  // Ask user if they want uppercase letters lowercase letters numbers and special characters
-    var upper = confirm("Would you like to use uppercase letters?"); 
-    var lower = confirm("Would you like to use lowercase letters?");
-    var enterNumber = confirm("Would you like to use numbers?");
-    var specialChar = confirm("Would you like to use special characters?");
 
-    while (!upper && !lower && !enterNumber && !specialChar) {
-      alert("Your must select at least one option");
-      var upper = confirm("Would you like to use uppercase letters?"); 
-      var lower = confirm("Would you like to use lowercase letters?");
-      var enterNumber = confirm("Would you like to use numbers?");
-      var specialChar = confirm("Would you like to use special characters?");
+  else {
+
+    // Ask user if they want uppercase letters, lowercase letters, numbers, and special characters
+    /*
+    var upperSelection = confirm("Would you like to use uppercase letters?");
+    var lowerSelection = confirm("Would you like to use lowercase letters?");
+    var number = confirm("Would you like to use numbers?");
+    var specialSelection = confirm("Would you like to use special characters?");
+    */
+
+    if (confirm("Would you like to use uppercase letters?") === true) {
+      upperSelection = true;
+       passwordLength = passwordLength.concat(upperCase);
     }
 
+    if (confirm("Would you like to use lowercase letters?") === true) {
+      lowerSelection = true;
+      passwordLength = passwordLength.concat(lowerCase);
+    }
 
+    if (confirm("Would you like to use numbers?") === true) {
+      numberSelection = true;
+      passwordLength = passwordLength.concat(number);
+    }
+
+    if (confirm("Would you like to use special characters?") === true) {
+      specialSelection = true;
+    passwordLength  = passwordLength.concat(specialCharacter);
+    }
+
+    console.log(passwordLength);
+    // If user does not select any of the given options
+    if (upperSelection === false && lowerSelection === false && numberSelection === false && specialSelection === false) {
+      window.alert("You must select at least one option");
+    }
+  }
+
+  // Generate password
+
+  // The users answers are randomized depending on what they selected
+  for (i = 0; i < characterLength; i++) {
+    pw.push(passwordLength[Math.floor(Math.random() * passwordLength.length)]);  
+  }
+
+  return pw.join("");
 }
 
-userChoice();
+  
 
 
-
-// Generate password
 // Put the generated password into the html file
-
-// Prompts user to input number and confirm. Log to console to verify input is stored.
-
-
-
-/*
-var upper = confirm("Would you like to use uppercase letters?");
-var lower = confirm("Would you like to use lowercase letters?");
-var enterNumber = confirm("Would you like to use numbers?");
-var specialChar = confirm("Would you like to use special characters?");
-
-
-
-
-
-
-
-
 
 
 // Get references to the #generate element
@@ -73,4 +97,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-*/
